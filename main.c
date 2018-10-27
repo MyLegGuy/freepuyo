799b0bb46b2f6cf013664b587ed94428d31994ed
MyLegGuy <supermoronslayer@gmail.com>
2018-10-27 01:49:37 -0400

    Lookididwork

--- main.c
+++ main.c
@@ -1,12 +1,32 @@
 #include <stdlib.h>
-#include <stdion.h>
+#include <stdio.h>
+
+#include <goodbrew/config.h>
+#include <goodbrew/base.h>
+#include <goodbrew/graphics.h>
+#include <goodbrew/controls.h>
+#include <goodbrew/images.h>
+
+typedef enum{
+	STATUS_NORMAL,
+	STATUS_POPPING,
+	STAUTS_DROPPING,
+	STATUS_NEXTWINDOW,
+	STATUS_TRASHFALL
+}boardStatus;
 
 typedef struct{
 	int w;
 	int h;
 	int** board; // ints represent colors
-	int** pieceStatus; // If pieces are popping
+	int** pieceStatus; // Interpret this value depending on status
+	boardStatus status;
+	int trashQueue;
 }puyoBoard;
+
+
+int screenWidth;
+int screenHeight;
 
 int** newJaggedArray(int _w, int _h){
 	int** _retArray = malloc(sizeof(int*)*_w);
@@ -20,9 +40,17 @@ void zeroJaggedArray(int** _zeroThis, int _w, int _h){
 	int i, j;
 	for (i=0;i<_w;++i){
 		for (j=0;j<_h;++j){
-			_zeroThis[_w][_h]=0;
+			_zeroThis[i][j]=0;
 		}
 	}
+}
+
+void resetBoard(puyoBoard* _passedBoard){
+	_passedBoard->status = STATUS_NORMAL;
+	_passedBoard->trashQueue=0;
+
+	zeroJaggedArray(_passedBoard->board,_passedBoard->w,_passedBoard->h);
+	zeroJaggedArray(_passedBoard->pieceStatus,_passedBoard->w,_passedBoard->h);
 }
 
 puyoBoard newBoard(int _w, int _h){
@@ -31,9 +59,37 @@ puyoBoard newBoard(int _w, int _h){
 	_retBoard.h = _h;
 	_retBoard.board = newJaggedArray(_w,_h);
 	_retBoard.pieceStatus = newJaggedArray(_w,_h);
-
-	zeroJaggedArray(_retBoard.board,_w,_h);
-	zeroJaggedArray(_retBoard.pieceStatus,_w,_h);
-
+	resetBoard(&_retBoard);
 	return _retBoard;
 }
+
+void XOutFunction(){
+	exit(0);
+}
+
+void init(){
+	generalGoodInit();
+	initGraphics(640,480,&screenWidth,&screenHeight);
+	initImages();
+	setWindowTitle("Test happy");
+}
+
+int main(int argc, char const** argv){
+	init();
+	puyoBoard _testBoard = newBoard(6,12);
+
+	while(1){
+		controlsStart();
+		controlsEnd();
+		startDrawing();
+		endDrawing();
+
+		// temp
+		wait(1);
+	}
+
+	generalGoodQuit();
+	return 0;
+}
+
+// use the bitmap autotiling
