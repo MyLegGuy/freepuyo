c0c6b5cfc2541b1cbf27cf8a7f817f1f5cd3bd8c
MyLegGuy <supermoronslayer@gmail.com>
2019-09-17 22:26:26 -0400

    free score constants

--- scoreConstants.h
+++ scoreConstants.h
@@ -1,15 +1,4 @@
-// tsu
 unsigned char colorCountBouns[6] = {0,3,6,12,24,48};
-unsigned char groupBonus[8] = {0,2,3,4,5,6,7,10};
-// tsu single player
-//short chainPowers[24] = {4,20,24,32,48,96,160,240,320,480,600,700,800,900,999,999,999,999,999,999,999,999,999,999};
-// tsu multiplayer
-short chainPowers[24] = {0,8,16,32,64,96,128,160,192,224,256,288,320,352,384,416,448,480,512,544,576,608,640,672};
-
-/*
-// fever
-unsigned char colorCountBouns[6] = {0,2,4,8,16,32}; // last number is made up
-unsigned char groupBonus[8] = {0,1,2,3,4,5,6,8};
-// temp. based on raffina's from ppf1
-short chainPowers[24] = {4,11,24,33,51,106,179,274,371,472,600,732,882,999,999,999,999,999,999};
-*/
+unsigned char groupBonus[8] = {0,2,3,4,5,6,7,12};
+short chainPowers[24] = {0,8,16,32,64,102,153,215,279,335,369,405,446,515,541,568,569,626,658,691,725,761,800,840};
+//							x2		  x1.6 x1.5 x1.4 x1.3 x1.2 x1.1 x1.1 x1.1 x1.05 x1.05 x1.05...
